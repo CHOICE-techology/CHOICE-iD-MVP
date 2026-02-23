fa91cf98a2d1d14080ada1ec57e380524421c08d
CHOICE <info@choice.love>
2026-02-23 23:05:43 +0200

    Delete choice-id directory

--- choice-id/fetch-shim.js
+++
@@ -1,7 +0,0 @@
-const fetch = window.fetch;
-const Request = window.Request;
-const Response = window.Response;
-const Headers = window.Headers;
-
-export { fetch, Request, Response, Headers };
-export default fetch;
